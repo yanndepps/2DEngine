5ff816f76cbcb2b3b04bf01fac43bcf32110636c
yanndepps <yann.deperrot@protonmail.com>
2022-01-23 19:22:27 +0100

    operator overloading

--- src/ECS/ECS.cpp
+++ src/ECS/ECS.cpp
@@ -6,11 +6,10 @@ int Entity::GetId() const { return id; }
 void System::AddEntityToSystem(Entity entity) { entities.push_back(entity); }
 
 void System::RemoveEntityFromSystem(Entity entity) {
-  entities.erase(std::remove_if(entities.begin(), entities.end(),
-                                [&entity](Entity other) {
-                                  return entity.GetId() == other.GetId();
-                                }),
-                 entities.end());
+  entities.erase(
+      std::remove_if(entities.begin(), entities.end(),
+                     [&entity](Entity other) { return entity == other; }),
+      entities.end());
 }
 
 std::vector<Entity> System::GetSystemEntities() const { return entities; }
--- src/ECS/ECS.h
+++ src/ECS/ECS.h
@@ -36,7 +36,14 @@ private:
 
 public:
   Entity(int id) : id(id){};
+  Entity(const Entity &entity) = default;
   int GetId() const;
+
+  Entity &operator=(const Entity &other) = default;
+  bool operator==(const Entity &other) const { return id == other.id; }
+  bool operator!=(const Entity &other) const { return id != other.id; }
+  bool operator>(const Entity &other) const { return id > other.id; }
+  bool operator<(const Entity &other) const { return id < other.id; }
 };
 
 //////////////////////////////////////////////////////////////////
