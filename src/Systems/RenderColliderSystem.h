919e796dbb2ac43ed586885dd61249029610f5a5
yanndepps <yann.deperrot@protonmail.com>
2023-05-22 14:19:45 +0200

    scale colliders

--- src/Systems/RenderColliderSystem.h
+++ src/Systems/RenderColliderSystem.h
@@ -24,8 +24,8 @@ public:
       SDL_Rect colliderRect = {
           static_cast<int>(transform.position.x + collider.offset.x - camera.x),
           static_cast<int>(transform.position.y + collider.offset.y - camera.y),
-          static_cast<int>(collider.width),
-          static_cast<int>(collider.height)};
+          static_cast<int>(collider.width * transform.scale.x),
+          static_cast<int>(collider.height * transform.scale.y)};
       SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
       SDL_RenderDrawRect(renderer, &colliderRect);
     }
